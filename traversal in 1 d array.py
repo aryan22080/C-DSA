a12ced4ed7d5b3a7bdbcaca759c0e518e32e8d6a
aryan22080 <85477366+aryan22080@users.noreply.github.com>
2025-05-23 16:10:40 +0530

    Add files via upload

---
+++ traversal in 1 d array.py
@@ -0,0 +1,5 @@
+import array 
+my_array1 = array.array('i',[1,2,3,4,5,6])
+print(my_array1)
+for i in range(len(my_array1)):
+    print(my_array1[i])
---
+++ traversal in 2d array.py
@@ -0,0 +1,6 @@
+import numpy as np 
+my_array2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(my_array2)
+for i in range(3):
+    for j in range(3):
+        print(my_array2[i][j])
